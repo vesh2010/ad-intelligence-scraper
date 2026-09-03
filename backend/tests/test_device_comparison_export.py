d8f494a26aba3ea5dd63b231a1bc9e8c8519b8cc
Avesh Mishra <52110530+vesh2010@users.noreply.github.com>
2026-09-03 23:08:16 +0530

    cleanup: remove redundant device comparison export test

--- backend/tests/test_device_comparison_export.py
+++
@@ -1,6 +0,0 @@
-from app.device_comparison import DESKTOP, MOBILE, compare_devices
-
-def test_exported_symbols():
-    assert DESKTOP.name == 'desktop'
-    assert MOBILE.name == 'mobile'
-    assert compare_devices([])['campaign_count'] == 0
