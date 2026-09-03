db4f79873966600d6cd5dc0077a6dd52f8250f1e
Avesh Mishra <52110530+vesh2010@users.noreply.github.com>
2026-09-03 23:07:43 +0530

    cleanup: remove obsolete duplicate device comparison test

--- backend/tests/test_device_compare_final.py
+++
@@ -1,5 +0,0 @@
-from app.device_compare_final import compare_devices
-
-def test_final_entrypoint():
-    r=compare_devices([{'campaign_key':'x','device':'desktop'},{'campaign_key':'x','device':'mobile'}])
-    assert r['both_device_campaigns']==1
